74d53794b425a3a7f1f3a239a2eb4722d4781656
Arseniy Markov <zamberg42@gmail.com>
2022-01-15 13:46:59 +0300

    20. Passing on Data with Events

--- src/app.js
+++ src/app.js
@@ -15,7 +15,11 @@ const vm = Vue.createApp({
         increment() {
             this.age++;
         },
-        updateLastName(event) {
+        updateLastName(message, event) {
+            event.preventDefault();
+
+            console.log(message);
+
             this.lastName = event.target.value;
         },
     },
