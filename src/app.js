ad43f80382d21013a6a86d53ee898d40f7a44ce3
Arseniy Markov <zamberg42@gmail.com>
2022-01-16 14:00:43 +0300

    section-2-19-complete

--- src/app.js
+++ src/app.js
@@ -1,2 +1,14 @@
 let vm = Vue.createApp({
-}).mount('#app')
+  data() {
+    return {
+      isPurple: false,
+      selectedColor: '',
+      size: 150
+    }
+  },
+  computed: {
+    circle_classes() {
+      return { purple: this.isPurple }
+    }
+  }
+}).mount('#app');
