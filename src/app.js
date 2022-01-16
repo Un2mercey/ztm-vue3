e117029749be52357c1e77ddb538e34966d40dfd
Arseniy Markov <zamberg42@gmail.com>
2022-01-16 13:58:06 +0300

    27. Binding Styles

--- src/app.js
+++ src/app.js
@@ -3,13 +3,30 @@ let vm = Vue.createApp({
         return {
             isPurple: false,
             selectedColor: "",
+            size: 150,
+            degree: 0,
         };
     },
     computed: {
         circle_classes() {
-            return {
-                purple: this.isPurple,
-            };
+            return [
+                this.selectedColor,
+                {
+                    purple: this.isPurple,
+                },
+            ];
+        },
+        circle_style() {
+            return [
+                {
+                    width: `${this.size}px`,
+                    height: `${this.size}px`,
+                    lineHeight: `${this.size}px`,
+                },
+                {
+                    transform: `rotate(${this.degree}deg)`,
+                }
+            ];
         },
     }
 }).mount('#app')
