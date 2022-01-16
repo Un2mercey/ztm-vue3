4592a98b8720a4b3a5c62dd675a2d9a0d7369d91
Arseniy Markov <zamberg42@gmail.com>
2022-01-16 13:43:42 +0300

    26. Binding Classes

--- src/app.js
+++ src/app.js
@@ -1,2 +1,15 @@
 let vm = Vue.createApp({
+    data() {
+        return {
+            isPurple: false,
+            selectedColor: "",
+        };
+    },
+    computed: {
+        circle_classes() {
+            return {
+                purple: this.isPurple,
+            };
+        },
+    }
 }).mount('#app')
