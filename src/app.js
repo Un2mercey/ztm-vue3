c8eea29c3740e2d1e7cb5f5b0b08b09628a38a70
Arseniy Markov <zamberg42@gmail.com>
2022-01-16 13:23:29 +0300

    section-2-17-complete

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const vm = Vue.createApp({
   data() {
     return {
       firstName: 'John',
+      middleName: '',
       lastName: 'Doe',
       url: 'https://google.com',
       raw_url: '<a href="https://google.com" target="_blank">Google</a>',
@@ -9,14 +10,30 @@ const vm = Vue.createApp({
     }
   },
   methods: {
-    fullName() {
-      return `${this.firstName} ${this.lastName.toUpperCase()}`
-    },
     increment() {
       this.age++
     },
-    updateLastName(event) {
+    updateLastName(msg, event) {      
+      // console.log(msg)
+
       this.lastName = event.target.value
+    },
+    updateMiddleName(event) {
+      this.middleName = event.target.value
+    }
+  },
+  computed: {
+    fullName() {
+      console.log('Full name computed property was called!')
+
+      return `${this.firstName} ${this.middleName} ${this.lastName.toUpperCase()}`
+    },
+  },
+  watch: {
+    age(newVal, oldVal) {
+      setTimeout(() => {
+        this.age = 20
+      }, 3000)
     }
   }
 }).mount('#app')
