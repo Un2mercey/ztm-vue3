24a78442bea7aab054032ff2a54726c7e0bfc56e
Arseniy Markov <zamberg42@gmail.com>
2022-01-17 20:20:05 +0300

    section-4-7-complete

--- src/section4/app.js
+++ src/section4/app.js
@@ -1,37 +1,28 @@
 let vm = Vue.createApp({
+  //  template: ``
+})
+
+vm.component('hello', {
+  template: `<h1>{{ message }}</h1>`,
   data() {
     return {
-      message: "Hello world!"
+      message: 'Hello World!'
     }
-  },
-  beforeCreate() {
-    console.log('beforeCreate() function called!', this.message)
-  },
-  created() {
-    console.log('created() function called!', this.message)
-  },
-  beforeMount() {
-    console.log('beforeMount() function called!', this.$el)
-  },
-  mounted() {
-    console.log('mounted() function called!', this.$el)
-  },
-  beforeUpdate() {
-    console.log('beforeUpdate() function called!')
-  },
-  updated() {
-    console.log('updated() funcction called!')
-  },
-  beforeUnmount() {
-    console.log('beforeUnmount() function called!')
-  },
-  unmounted() {
-    console.log('unmounted() function called!')
   }
 })
 
 vm.mount('#app')
 
-// setTimeout(() => {
-//   vm.mount('#app')
-// }, 3000)
+// let vm2 = Vue.createApp({
+//   data() {
+//     return {
+//       message: "Hello world!"
+//     }
+//   },
+//   render() {
+//     return Vue.h(
+//       'h1',
+//       this.message
+//     )
+//   }
+// }).mount("#app2")
