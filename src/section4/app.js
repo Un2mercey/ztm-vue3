d5edd1ab96a24e1cc5eb5f5c8d08e96133285e3e
Arseniy Markov <zamberg42@gmail.com>
2022-01-17 17:05:18 +0300

    section-4-3-complete

--- src/section4/app.js
+++ src/section4/app.js
@@ -3,5 +3,35 @@ let vm = Vue.createApp({
     return {
       message: "Hello world!"
     }
+  },
+  beforeCreate() {
+    console.log('beforeCreate() function called!', this.message)
+  },
+  created() {
+    console.log('created() function called!', this.message)
+  },
+  beforeMount() {
+    console.log('beforeMount() function called!', this.$el)
+  },
+  mounted() {
+    console.log('mounted() function called!', this.$el)
+  },
+  beforeUpdate() {
+    console.log('beforeUpdate() function called!')
+  },
+  updated() {
+    console.log('updated() funcction called!')
+  },
+  beforeUnmount() {
+    console.log('beforeUnmount() function called!')
+  },
+  unmounted() {
+    console.log('unmounted() function called!')
   }
-}).mount('#app')
+})
+
+vm.mount('#app')
+
+// setTimeout(() => {
+//   vm.mount('#app')
+// }, 3000)
