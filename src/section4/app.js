4441dbb16318e0e14e49f9a9bae1eb6001cf7489
Arseniy Markov <zamberg42@gmail.com>
2022-01-17 17:02:34 +0300

    39. Using Lifecycle Hooks

--- src/section4/app.js
+++ src/section4/app.js
@@ -3,7 +3,63 @@ let vm = Vue.createApp({
         return {
             message: "Hello world!"
         }
-    }
+    },
+
+    /**
+     * @hook
+     */
+    beforeCreate() {
+        console.log("beforeCreate() function called!\nmessage in data():", this.message);
+    },
+
+    /**
+     * @hook
+     */
+    created() {
+        console.log("created() function called!\nmessage in data():", this.message);
+    },
+
+    /**
+     * @hook
+     */
+    beforeMount() {
+        console.log("beforeMount() function called!\n$el", this.$el);
+    },
+
+    /**
+     * @hook
+     */
+    mounted() {
+        console.log("mounted() function called!\n$el:", this.$el);
+    },
+
+    /**
+     * @hook
+     */
+    beforeUpdate() {
+        console.log("beforeUpdate() function called!");
+    },
+
+    /**
+     * @hook
+     */
+    updated() {
+        console.log("updated() function called!");
+    },
+
+    /**
+     * @hook
+     */
+    beforeUnmount() {
+        console.log("beforeUnmount() function called!");
+    },
+
+    /**
+     * @hook
+     */
+    unmounted() {
+        console.log("unmounted() function called!");
+    },
 });
 
 vm.mount('#app')
