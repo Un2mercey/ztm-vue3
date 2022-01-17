7af6592f7a7a2f5f9d04b11a50f580e7395c13b7
Arseniy Markov <zamberg42@gmail.com>
2022-01-17 13:12:05 +0300

    35. Copying to the Clipboard

--- src/app.js
+++ src/app.js
@@ -5,17 +5,14 @@ Vue.createApp({
             rotateX: 0,
             rotateY: 0,
             rotateZ: 0,
+            isSnackbarVisible: false,
+            timeoutFn: null,
         };
     },
     computed: {
         box() {
             return {
-                transform: `
-                    perspective(${this.perspective}px)
-                    rotateX(${this.rotateX}deg)
-                    rotateY(${this.rotateY}deg)
-                    rotateZ(${this.rotateZ}deg)
-                `,
+                transform: `perspective(${this.perspective}px) rotateX(${this.rotateX}deg) rotateY(${this.rotateY}deg) rotateZ(${this.rotateZ}deg)`,
             };
         },
     },
@@ -26,5 +23,21 @@ Vue.createApp({
             this.rotateY = 0;
             this.rotateZ = 0;
         },
+        copy() {
+            this.isSnackbarVisible = true;
+            clearTimeout(this.timeoutFn);
+            const textareaEl = document.createElement("textarea");
+            textareaEl.setAttribute("readonly", "");
+            textareaEl.style.position = "absolute";
+            textareaEl.style.left = "-9999px";
+            textareaEl.value = `transform: ${this.box.transform};`;
+            document.body.appendChild(textareaEl);
+            textareaEl.select();
+            document.execCommand("copy");
+            document.body.removeChild(textareaEl);
+            this.timeoutFn = setTimeout(() => {
+                this.isSnackbarVisible = false;
+            }, 2000);
+        }
     },
 }).mount("#app")
