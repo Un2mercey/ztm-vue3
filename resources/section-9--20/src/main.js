29d8ade77546b037ccbf9344af211543e9469d8d
Arseniy Markov <zamberg42@gmail.com>
2022-01-20 10:27:01 +0300

    section-10-12-starter

--- resources/section-9--20/src/main.js
+++ resources/section-9--20/src/main.js
@@ -1,15 +1,4 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router';
-import store from './store';
-import VeeValidatePlugin from './includes/validation';
-import './assets/tailwind.css';
-import './assets/main.css';
+import { createApp } from 'vue'
+import App from './App.vue'
 
-const app = createApp(App);
-
-app.use(store);
-app.use(router);
-app.use(VeeValidatePlugin);
-
-app.mount('#app');
+createApp(App).mount('#app')
