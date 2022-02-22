d856a9f9ed132408fddd57d2771f0886a4bd1a81
Arseniy Markov <zamberg42@gmail.com>
2022-02-22 09:51:17 +0300

    150. Navigating with Links

--- resources/section-9--20/src/components/layout/AppHeader.vue
+++ resources/section-9--20/src/components/layout/AppHeader.vue
@@ -1,9 +1,12 @@
 <template>
     <header id="header" class="bg-gray-700">
         <nav class="container mx-auto flex justify-start items-center py-5 px-4">
-            <a class="text-white font-bold uppercase text-2xl mr-4" href="#">
+            <router-link
+                class="text-white font-bold uppercase text-2xl mr-4"
+                to="/about"
+            >
                 Music
-            </a>
+            </router-link>
             <div class="flex flex-grow items-center">
                 <ul class="flex flex-row mt-1">
                     <li v-if="!user">
